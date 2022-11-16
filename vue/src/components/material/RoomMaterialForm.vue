13a5be11fc7662119cc6c1e4b1a3e4fcc6748699
nvlight <lightstyle11@mail.ru>
2022-11-16 20:38:04 +0300

    reworked roomMaterialForm - now list is filtered

--- vue/src/components/material/RoomMaterialForm.vue
+++ vue/src/components/material/RoomMaterialForm.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="max-w-md w-full m-auto">
         <h1 class="mt-3 font-semibold text-2xl text-center">Добавление материалов</h1>
+
+        <div>
+            <mg-input-labeled v-model="searchMaterialQuery">фильтр материалов</mg-input-labeled>
+        </div>
+
         <div class="pt-1">
             <div class="">
+<!--                <div>{{this.selectedMaterial}}</div>-->
+<!--                <div>{{+this.selectedMaterial[0]}}</div>-->
+
                 <label for="material_name" class="text-sm text-center font-medium text-gray-700">Наименование
                 </label>
 
                 <select v-model="selectedMaterial"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                            focus:ring-blue-500 focus:border-blue-500
-                            block w-full p-2.5
-                            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            >
-                    <option value="0">Выберите из списка</option>
+                    multiple
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                        focus:ring-blue-500 focus:border-blue-500
+                        block w-full p-2.5
+                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        >
+<!--                    <option value="0">Выберите из списка</option>-->
                     <option
-                        v-for="option in materials"
+                        v-for="option in filteredMaterials"
                         :key="option.id"
                         :value="option.id"
                     >
@@ -61,8 +70,10 @@ export default {
     },
     data() {
         return {
-            selectedMaterial: "1",
+            selectedMaterial: [],
             amount: 1,
+
+            searchMaterialQuery: '',
         }
     },
     methods:{
@@ -84,8 +95,13 @@ export default {
             roomMaterials: "roomMaterial/roomMaterials",
         }),
 
+        filteredMaterials(){
+            return this.materials.filter(t =>
+                t.title.toLowerCase().includes(this.searchMaterialQuery.toLowerCase()) )
+        },
+
         materialPrice(){
-            const find = this.materials.filter( t => t.id === +this.selectedMaterial)
+            const find = this.materials.filter( t => t.id === +this.selectedMaterial[0])
             if (!find.length){
                 return 0;
             }
@@ -98,7 +114,7 @@ export default {
 
         roomMaterial(){
             const room_id     = this.room_id;
-            const material_id = this.selectedMaterial;
+            const material_id = this.selectedMaterial[0];
             const amount      = this.amount;
             const sum         = this.sum;
 
